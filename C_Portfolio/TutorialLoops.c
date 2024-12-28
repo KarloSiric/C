b937c43fb36cfe484e37757e5e4a33bd75534f64
KarloSiric <ksiric99@gmail.com>
2024-12-28 14:00:27 +0100

    adding tutorial loops

---
+++ C_Portfolio/TutorialLoops.c
@@ -0,0 +1,31 @@
+/*
+ * @Author: karlosiric
+ * @Date:   2024-12-28 13:57:46
+ * @Last Modified by:   karlosiric
+ * @Last Modified time: 2024-12-28 14:00:05
+ */
+
+/*
+    This will be a tutorial on Loops in C some short guide nothing much,
+    And I am following a guide from Potrfolio Courses on YT for all of this.
+    He also has a guide on solving problems in C and I will be documenting that
+   too. Some prior basics are missing I might add them later on but for now
+   loops and forward stuff only.
+
+*/
+
+#include <stdio.h>
+int main(void) {
+    
+    int number = 0;
+    
+    while (number < 10) {
+        printf("Number is: %d\n", number);
+        number++;
+        
+    }
+    
+    
+    
+    return 0;
+}

37346862abfa24f6859421266406d931021a2159
KarloSiric <ksiric99@gmail.com>
2025-01-13 15:39:07 +0100

    working on a switch statement programs

--- C_Portfolio/switch.c
+++ C_Portfolio/switch.c
@@ -2,7 +2,7 @@
 * @Author: karlosiric
 * @Date:   2025-01-13 10:55:51
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2025-01-13 15:00:45
+* @Last Modified time: 2025-01-13 15:38:50
 */
 
 // new problem, discussin switch statements
@@ -15,8 +15,11 @@ int main(void) {
     switch (number) {
         case 0:
             printf("Case 0!\n");
+        case 1:
+            printf("Case 1!\n");
+        case 2:
+            printf("Case 2!\n");
     } 
-    
     
     
     return 0;

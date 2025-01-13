80bf12d761677d2c11c2cded0a5ba1502f5f2f30
KarloSiric <ksiric99@gmail.com>
2025-01-13 15:01:26 +0100

    working on a switch statement programs

---
+++ C_Portfolio/switch.c
@@ -0,0 +1,24 @@
+/*
+* @Author: karlosiric
+* @Date:   2025-01-13 10:55:51
+* @Last Modified by:   karlosiric
+* @Last Modified time: 2025-01-13 15:00:45
+*/
+
+// new problem, discussin switch statements
+
+#include <stdio.h>
+int main(void) {
+    
+    int number = 0;
+    
+    switch (number) {
+        case 0:
+            printf("Case 0!\n");
+    } 
+    
+    
+    
+    return 0;
+    
+}

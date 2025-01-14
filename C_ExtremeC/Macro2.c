bef90e15084ec84cf8b585193703620291869517
KarloSiric <ksiric99@gmail.com>
2025-01-14 14:40:26 +0100

    Added another Macro example, macro2.c

--- C_ExtremeC/Macro2.c
+++ C_ExtremeC/Macro2.c
@@ -2,7 +2,7 @@
 * @Author: karlosiric
 * @Date:   2025-01-14 14:32:20
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2025-01-14 14:34:16
+* @Last Modified time: 2025-01-14 14:39:49
 */
 
 /*
@@ -19,7 +19,35 @@ int main(int argc, char const *argv[])
     int x = 2;
     int y = 3;
     
-    printf("The sum of x and y is: %d\n", ADD(x,y));
+    int z = ADD(x, y);
+    
+    printf("The sum of x and y is: %d\n", z);
     
     return 0;
 }
+
+/*
+    Here ADD is not a function per say but rather
+    it is a macro defined with arguments.
+    When the preprocessing is finished,
+    and after macro expansion, we get
+    a code that looks like the following:
+    
+    int main(int argc, char const *argv[]) {
+        
+        int x = 2;
+        int y = 3;
+        
+        int z = x + y;
+        
+        printf("...");
+    
+        return 0;
+    }
+
+    So what happens is that the preprocessor replaces the name of the macro used with it values
+    or in this case arguments defined.
+    
+    
+    
+ */
